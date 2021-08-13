6313f531f6c9ae43e0623dda0f0028ed4b27a015
Michael Sanchez <michaelsanchez42@live.com>
2021-08-12 21:28:33 -0600

    edit contact

---
+++ src/Contact.js
@@ -0,0 +1,21 @@
+import React from "react"
+import "./contact.css"
+
+function Contact(props) {
+    return (
+        <>
+        <h1>Contact</h1>
+        <span className="span"></span>
+        <div className="content">
+            <p>Social Media</p>
+            <div id="social-media">
+                <a
+                href=""
+                alt="LinkedIn"
+                title="LinkedIn"
+                ></a>
+            </div>
+        </div>
+        </>
+    )
+}
